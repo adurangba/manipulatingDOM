2ebb656f1ee666f9972c6acbb2abe788f988d550
adurangba <isaacabioye5@gmail.com>
2024-05-04 02:19:20 +0100

    restudying Javascript DOM

---
+++ script.js
@@ -0,0 +1,31 @@
+// DOM Manaipulation
+
+// Traverse the DOM
+
+//Parent Node Traversal
+// let ul = document.querySelector("ul");
+
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
+
+// const html = document.documentElement;
+
+// console.log(html.parentNode);
+// console.log(html.parentElement);
+
+//Child Node Traversal
+
+// let ul = document.querySelector("ul");
+
+// console.log(ul.childNodes)
+// ul.childNodes[1].style.backgroundColor = "red"
+// console.log(ul.children)
+
+//Sibling Node Traversal 
+let ul = document.querySelector("ul");
+div = document.querySelector("div")
+
+console.log(div.childNodes)
+
+
+
